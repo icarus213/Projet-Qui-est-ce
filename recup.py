d5ef0ad8fada8616755a0c57bc1c6e2dc2cb7b75
icarus213 <79603405+icarus213@users.noreply.github.com>
2022-02-19 20:26:51 +0100

    Add files via upload

--- recup.py
+++ recup.py
@@ -1,10 +1,44 @@
+from tkinter import *
+import tkinter as tk
 import json
+from PIL import ImageTk
+mes_images = []
+nbrCol = 0
+nbrLi = 0
 
-
-def test():
-
+def affichage_possibilite():
+        
+    mes_perso = []
+    with open('perso.json', 'r') as f:
+        data = json.load(f)
+    possibilites = data["possibilites"]
+    for i in data["possibilites"]:
+        perso = possibilites[str(i)]
+        my_img = "personnages/" + perso["fichier"]
+        mes_images.append(my_img)
+        mes_perso.append(perso["prenom"])
+    
+def recup_keys(): #retourne la liste des keys qui décrivent les perso: exemple: ['fichier', 'prenom', 'genre', 'cheveux', 'lunettes', 'chauve']
   with open('perso.json', 'r') as f:
     data = json.load(f)
+    possibilites = data["possibilites"]
+    
+    infoPerso = possibilites["0"]
+    keys = list(infoPerso)  #liste des keys du dictionnaire
 
-  # Output: {'name': 'Bob', 'languages': ['English', 'French']}
-  print(data)
+    # for i in range(len(keys)) :  #affiche toutes les key du dictionnaire
+    #   print(keys[i])
+
+    return keys 
+
+
+
+def nbrColonne():
+  with open('perso.json', 'r') as f:
+        data = json.load(f) 
+  return data["colonne"]
+
+def nbrLigne(): 
+  with open('perso.json', 'r') as f:
+        data = json.load(f)
+  return data["ligne"]
